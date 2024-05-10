e3ec5bc2db58603e7c9131ee4b9f8969104ac34c
LuisCoronel2005 <156546596+LuisCoronel2005@users.noreply.github.com>
2024-05-10 02:33:28 -0400

    Update cli_network_manager.py

--- cli_network_manager.py
+++ cli_network_manager.py
@@ -1,21 +1,4 @@
-"""
-Author: Luke Trujillo & Luis Coronel.
-Date: 2024-05-09
-Version: 1.0
-
-This command-line network manager is a Python program designed to manage network traffic.
-
-Classes:
-1. Network Manager
-    - Controls interactions with the network
-
-2. CLI:
-    - Provides a command-line interface for users to interact with the network manager
-
-Dependencies:
-    - scapy: Packet manipulation and analysis.
-"""
-
+from os import system
 from scapy.all import *
 import threading
 
@@ -68,10 +51,15 @@ class NetworkManager:
                 print(f"{counter}: {packet.summary()}")
         print("{ END } ------------------------------------------------------------")
 
+    def pcap_gen(self):
+        self.filename = input("What name do you want for your file name: ")
+        system('sudo tcpdump -i wlan0 -w ' + str(self.filename))
+        system('^C')
+        
     def scan_network(self):
         print("{ ACTIVE HOSTS } -----------------------------------------------------")
         ip_mac = {}
-        ans, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst="192.168.1.0/24"), timeout=2, verbose=True)
+        ans, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst="192.168.1.0/24"), timeout=2, verbose=1)
         for _, rcv in ans:
             ip_mac[rcv.psrc] = rcv.sprintf("%Ether.src%")
         print("{ END } ------------------------------------------------------------")
@@ -97,37 +85,42 @@ class CLI:
         print("  \033[32mhelp:\033[0m Return a list of available commands")
         print("  \033[32mstart:\033[0m Start packet sniffing")
         print("  \033[32mstop:\033[0m Stop packet sniffing")
-        print("  \033[32mpacket:\033[0m Print captured packets (only when not sniffing)")
+        print("  \033[32mpacket:\033[0m Print captured packets")
         print("  \033[32mscan:\033[0m Scan for active hosts in the network")
         print("  \033[32msend <source_ip> <destination_ip>:\033[0m Send a packet (optional source IP)")
         print("  \033[32msyn_flood <target_ip>:\033[0m Perform a SYN flood attack on a target IP address")
+        print("  \033[32mpcap_gen:\033[0m Generate a Pcap file")
 
     def run(self):
         print(f"\033[34mType 'help' for available commands.\033[0m")
         while True:
             command = input("\033[34mEnter a command: \033[0m").strip().split(' ', 1)
-            if command[0] == "help":
+            action = command[0]
+            args = command[1:] if len(command) > 1 else []
+            if action == "help":
                 self.print_help()
-            elif command[0] == "start":
+            elif action == "start":
                 self.network_manager.start()
-            elif command[0] == "stop":
+            elif action == "stop":
                 self.network_manager.stop()
-            elif command[0] == "packet":
+            elif action == "pcap_gen":
+                self.network_manager.pcap_gen()
+            elif action == "packet":
                 self.network_manager.print_packet()
-            elif command[0] == "scan":
+            elif action == "scan":
                 self.network_manager.scan_network()
-            elif command[0] == "send":
-                if len(command) != 2:
+            elif action == "send":
+                if len(args) != 1:
                     print("Please provide both source and destination IP addresses.")
                 else:
-                    src_ip, dst_ip = command[1].split()
+                    src_ip, dst_ip = args[0].split()
                     packet = IP(src=src_ip, dst=dst_ip)/TCP(dport=80)
                     self.network_manager.send_packet(packet, src_ip)
-            elif command[0] == "syn_flood":
-                if len(command) != 2:
+            elif action == "syn_flood":
+                if len(args) != 1:
                     print("Please provide the target IP address.")
                 else:
-                    target_ip = command[1]
+                    target_ip = args[0]
                     self.network_manager.syn_flood(target_ip)
             else:
                 print("Invalid command. Type 'help' for available commands.")
